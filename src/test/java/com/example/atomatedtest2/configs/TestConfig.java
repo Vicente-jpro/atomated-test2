dc30c7f62046c2a6d44ce3ea1c8431f5cdf6982e
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2024-08-08 22:32:02 +0100

    Testcontainer removed

--- src/test/java/com/example/atomatedtest2/configs/TestConfig.java
+++
@@ -1,7 +0,0 @@
-package com.example.atomatedtest2.configs;
-
-public class TestConfig {
-
-    public static final int SERVER_PORT = 8888;
-    public static final String CONTENT_TYPE_JSON = "application/json";
-}
